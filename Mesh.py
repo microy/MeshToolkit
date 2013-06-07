15df20a01607116ab67ed47388e85bcc477effc4
Michaël Roy <microygh@gmail.com>
2013-06-07 11:33:51 +0200

    Split code into several files.

--- Mesh.py
+++ Mesh.py
@@ -18,11 +18,7 @@
 # ***************************************************************************
 
 
-#
-# External dependencies
-#
-import math
-import numpy
+
 
 
 #--
@@ -73,55 +69,5 @@ class Mesh :
 	#
 	def FaceNumber( self ) :
 		return len( self.faces )
-
-
-
-
-#--
-#
-# UpdateNormals
-#
-#--
-def UpdateNormals( mesh ) :
-	# Calculate the normal for all the triangles, by taking the cross product of the vectors v1-v0, and v2-v0 in each triangle             
-	mesh.face_normals = numpy.cross( mesh.vertices[ mesh.faces[:,1] ] - mesh.vertices[ mesh.faces[:,0] ],
-					mesh.vertices[ mesh.faces[:,2] ] - mesh.vertices[ mesh.faces[:,0] ] )
-	# Initialize the vertex normal array
-	mesh.vertex_normals = numpy.zeros( (len(mesh.vertices), 3) )
-	# Add face normals to the normal of their respective vertices
-	for ( i, face ) in enumerate( mesh.faces ) :
-		mesh.vertex_normals[face] += mesh.face_normals[i]            
-	# Normalize the normal vectors
-#	mesh.face_normals /= numpy.apply_along_axis( numpy.linalg.norm, 1, mesh.face_normals ).repeat( 3 ).reshape( mesh.face_normals.shape )
-#	mesh.vertex_normals /= numpy.apply_along_axis( numpy.linalg.norm, 1, mesh.vertex_normals ).repeat( 3 ).reshape( mesh.vertex_normals.shape )
-	mesh.face_normals /= numpy.apply_along_axis( numpy.linalg.norm, 1, mesh.face_normals )[:,numpy.newaxis]
-	mesh.vertex_normals /= numpy.apply_along_axis( numpy.linalg.norm, 1, mesh.vertex_normals )[:,numpy.newaxis]
-	return mesh
-
-#--
-#
-# UpdateNeighbors
-#
-#--
-def UpdateNeighbors( mesh ) :
-	# Initialization
-	mesh.neighbor_vertices = [ [] for i in xrange(mesh.VertexNumber()) ]
-	mesh.neighbor_faces = [ [] for i in xrange(mesh.VertexNumber()) ]
-	# Create a list of faces and vertices in the neighborhood
-	# for every vertex of the mesh
-	for i in range( mesh.FaceNumber() ) :
-		mesh.neighbor_faces[ mesh.faces[i,0] ].append( i )
-		mesh.neighbor_faces[ mesh.faces[i,1] ].append( i )
-		mesh.neighbor_faces[ mesh.faces[i,2] ].append( i )
-		mesh.neighbor_vertices[ mesh.faces[i,0] ].append( mesh.faces[i,1] )
-		mesh.neighbor_vertices[ mesh.faces[i,0] ].append( mesh.faces[i,2] )
-		mesh.neighbor_vertices[ mesh.faces[i,1] ].append( mesh.faces[i,0] )
-		mesh.neighbor_vertices[ mesh.faces[i,1] ].append( mesh.faces[i,2] )
-		mesh.neighbor_vertices[ mesh.faces[i,2] ].append( mesh.faces[i,0] )
-		mesh.neighbor_vertices[ mesh.faces[i,2] ].append( mesh.faces[i,1] )
-	# Remove duplicates
-	mesh.neighbor_vertices = [ list( set( i ) ) for i in mesh.neighbor_vertices ]
-	mesh.neighbor_faces = [ list( set( i ) ) for i in mesh.neighbor_faces ]
-	return mesh
 
 
---
+++ Neighborhood.py
@@ -0,0 +1,56 @@
+# -*- coding:utf-8 -*- 
+
+# ***************************************************************************
+#                               Neighborhood.py
+#                             -------------------
+#    update               : 2013-06-07
+#    copyright            : (C) 2013 by Michaël Roy
+#    email                : microygh@gmail.com
+# ***************************************************************************
+
+# ***************************************************************************
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU General Public License as published by  *
+# *   the Free Software Foundation; either version 2 of the License, or     *
+# *   (at your option) any later version.                                   *
+# *                                                                         *
+# ***************************************************************************
+
+
+#
+# External dependencies
+#
+import math
+import numpy
+from Mesh import Mesh
+
+
+
+#--
+#
+# UpdateNeighbors
+#
+#--
+def UpdateNeighbors( mesh ) :
+	# Initialization
+	mesh.neighbor_vertices = [ [] for i in xrange(mesh.VertexNumber()) ]
+	mesh.neighbor_faces = [ [] for i in xrange(mesh.VertexNumber()) ]
+	# Create a list of faces and vertices in the neighborhood
+	# for every vertex of the mesh
+	for i in range( mesh.FaceNumber() ) :
+		mesh.neighbor_faces[ mesh.faces[i,0] ].append( i )
+		mesh.neighbor_faces[ mesh.faces[i,1] ].append( i )
+		mesh.neighbor_faces[ mesh.faces[i,2] ].append( i )
+		mesh.neighbor_vertices[ mesh.faces[i,0] ].append( mesh.faces[i,1] )
+		mesh.neighbor_vertices[ mesh.faces[i,0] ].append( mesh.faces[i,2] )
+		mesh.neighbor_vertices[ mesh.faces[i,1] ].append( mesh.faces[i,0] )
+		mesh.neighbor_vertices[ mesh.faces[i,1] ].append( mesh.faces[i,2] )
+		mesh.neighbor_vertices[ mesh.faces[i,2] ].append( mesh.faces[i,0] )
+		mesh.neighbor_vertices[ mesh.faces[i,2] ].append( mesh.faces[i,1] )
+	# Remove duplicates
+	mesh.neighbor_vertices = [ list( set( i ) ) for i in mesh.neighbor_vertices ]
+	mesh.neighbor_faces = [ list( set( i ) ) for i in mesh.neighbor_faces ]
+	return mesh
+
+
