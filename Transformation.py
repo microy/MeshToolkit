fe145fd87db777d9eeb361688d502b1b3ec4b2e1
Michaël Roy <microygh@gmail.com>
2013-11-13 09:39:52 +0100

    Add a new Model-View-Projection matrix tool.

---
+++ Transformation.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*- 
+
+# ***************************************************************************
+#                              Transformation.py
+#                             -------------------
+#    update               : 2013-11-13
+#    copyright            : (C) 2013 by Michaël Roy
+#    email                : microygh@gmail.com
+# ***************************************************************************
+
+# ***************************************************************************
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU General Public License as published by  *
+# *   the Free Software Foundation; either version 2 of the License, or     *
+# *   (at your option) any later version.                                   *
+# *                                                                         *
+# ***************************************************************************
+
+
+#
+# External dependencies
+#
+from numpy import *
+
+
