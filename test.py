920e3caecccc5ff1c4dcd19b378b63fda71ff7f2
Michaël Roy <microygh@gmail.com>
2013-11-18 00:23:20 +0100

    Add QtViewer.

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ from Core import *
 from Core.Vrml import *
 from Viewer import *
 from Viewer.GlutViewer import *
+from Viewer.QtViewer import *
+import sys
 
 
 if __name__ == "__main__":
@@ -14,6 +16,13 @@ if __name__ == "__main__":
 #	UpdateNeighbors( m )
 	print m
 #	CheckMesh( m )
-	v = GlutViewer( mesh=m, title="Test" )
-	v.PrintInfo()
-	v.Run()
+#	v = GlutViewer( mesh=m, title="Test" )
+#	v.PrintInfo()
+#	v.Run()
+	
+
+	app = QtGui.QApplication( sys.argv )
+	window = QtViewer( mesh=m, title="Test" )
+	window.show()
+	sys.exit( app.exec_() )
+
