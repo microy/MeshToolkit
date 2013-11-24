906ebae3e5ee04f23a56a635a1817ec303b40b7b
Michaël Roy <microygh@gmail.com>
2013-11-24 01:22:26 +0100

    Remove unnecessary functions.

--- Viewer/Transformation.py
+++ Viewer/Transformation.py
@@ -3,7 +3,7 @@
 # ***************************************************************************
 #                              Transformation.py
 #                             -------------------
-#    update               : 2013-11-23
+#    update               : 2013-11-24
 #    copyright            : (C) 2013 by Michaël Roy
 #    email                : microygh@gmail.com
 # ***************************************************************************
@@ -23,8 +23,6 @@
 #
 #	- OpenGL and GLU documentation
 #         http://www.opengl.org/sdk/docs/man2
-#	  glTranslate, glRotate, glScale, glOrtho, glFrustrum
-#	  gluPerspective, gluLookAT
 #
 #	- GLM
 #	  https://github.com/g-truc/glm
@@ -40,9 +38,9 @@
 #
 #--
 #
-from math import *
-from numpy import *
-from numpy.linalg import *
+from math import tan, pi
+from numpy import identity, dot, float32
+
 
 
 
@@ -61,57 +59,6 @@ def TranslateMatrix( M, direction ) :
 	return dot( M, T )
 
 
-
-#--
-#
-# ScaleMatrix
-#
-#--
-#
-def ScaleMatrix( M, x, y=None, z=None ) :
-
-	if y is None : y = x
-	if z is None : z = x
-
-	S = zeros( (4, 4), dtype=float32 )
-	S[0] = dot( M[0], x )
-	S[1] = dot( M[1], y )
-	S[2] = dot( M[2], z )
-	S[3] = M[3]
-# Slower ?
-#	S = identity( 4, dtype=float32 )
-#	S[0,0] = x
-#	S[1,1] = y
-#	S[2,2] = z
-#	return dot( M, S )
-
-	return S
-
-
-#--
-#
-# RotateMatrix
-#
-#--
-#
-def RotateMatrix( M, angle, axis ) :
-
-	x, y, z = axis[0], axis[1], axis[2]
-	angle = pi * float(angle) / 180.0
-	c, s = cos( angle ), sin( angle )
-	n = sqrt( x*x + y*y + z*z )
-	if n == 0 : n = 1.0
-	x /= n
-	y /= n
-	z /= n
-	cx, cy, cz = (1 - c) * x, (1 - c) * y, (1 - c) * z
-
-	R = array([[  cx*x + c , cy*x - z*s, cz*x + y*s, 0],
-		   [ cx*y + z*s,   cy*y + c, cz*y - x*s, 0],
-		   [ cx*z - y*s, cy*z + x*s,   cz*z + c, 0],
-		   [          0,          0,          0, 1] ])
-
-	return array( dot( M, R ), dtype=float32 )
 
 
 #--
@@ -136,29 +83,6 @@ def OrthographicMatrix( left, right, bottom, top, znear, zfar ) :
 	# Return result
 	return M
 
-        
-#--
-#
-# FrustrumMatrix
-#
-#--
-#
-def FrustrumMatrix( left, right, bottom, top, znear, zfar ) :
-
-	# Initialise matrix
-	M = zeros( (4,4), dtype=float32 )
-
-	# Compute frustrum matrix
-	M[0,0] = 2.0 * float(znear) / float(right - left)
-	M[1,1] = 2.0 * float(znear) / float(top - bottom)
-	M[0,2] = float(right + left) / float(right - left)
-	M[1,2] = float(top + bottom) / float(top - bottom)
-	M[2,2] = - float(zfar + znear) / float(zfar - znear)
-	M[3,2] = - 1.0
-	M[2,3] = - 2.0 * float(znear) * float(zfar) / float(zfar - znear)
-
-	# Return result
-	return M
 
 
 #--
@@ -182,46 +106,5 @@ def PerspectiveMatrix( fovy, aspect, znear, zfar ) :
 
 	# Return result
 	return M
-
-
-#--
-#
-# LookAtMatrix
-#
-#--
-#
-def LookAtMatrix( eye, center, up ) :
-
-	# Cast input variables
-	eye = array ( eye, dtype=float32 )
-	center = array ( center, dtype=float32 )
-	up = array ( up, dtype=float32 )
-
-	# The "look-at" vector
-	f = center - eye
-	f /= norm( f )
-
-	# The "right" vector
-	s = cross(f, up)
-	s /= norm( s )
-
-	# The "up" vector
-	u = cross(s, f)
-
-	# Compute the "look-at" matrix
-	M = identity( 4, dtype=float32 )
-	M[0,0] =  s[0]
-	M[0,1] =  s[1]
-	M[0,2] =  s[2]
-	M[1,0] =  u[0]
-	M[1,1] =  u[1]
-	M[1,2] =  u[2]
-	M[2,0] = -f[0]
-	M[2,1] = -f[1]
-	M[2,2] = -f[2]
-
-	# Return result
-	return TranslateMatrix( M, -eye )
-
 
 
