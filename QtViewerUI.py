5ff5b5634372e2428e4dedb3a09863a56743c9ff
Michaël Roy <microygh@gmail.com>
2014-04-26 18:51:54 +0200

    Cleanup.

--- QtViewerUI.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'QtViewerUI.ui'
-#
-# Created: Sat Apr 26 04:47:20 2014
-#      by: pyside-uic 0.2.13 running on PySide 1.1.1
-#
-# WARNING! All changes made in this file will be lost!
-
