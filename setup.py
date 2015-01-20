d7e0517d0384f02f6aecfa913384fdfb873a69f3
Michaël Roy <microygh@gmail.com>
2015-01-20 13:33:51 +0100

    Add setup tools.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*- 
+
+#
+# Setup for the PyMeshToolkit package
+#
+
+# External dependencies
+from setuptools import setup, find_packages
+
+# Setup configuration
+setup(
+
+    name = "PyMeshToolkit",
+    version = "0.1dev",
+    packages = find_packages(),
+    author = "Michaël Roy",
+    author_email = "microygh@gmail.com",
+    description = "Python 3D Mesh Toolkit",
+    license = "MIT",
+    url = "https://github.com/microy/PyMeshToolkit",
+
+)
